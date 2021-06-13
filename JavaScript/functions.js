1255c23fbd8b86577fb11f973aeb7cca7316ebe8
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-06-13 11:18:58 -0400

    Learned how to write functions in javaScript

---
+++ JavaScript/functions.js
@@ -0,0 +1,18 @@
+// Writing Your Own Functions 
+
+// Simple Square Function in Javascript
+function square(x) {
+  var ans = x * x;
+  return ans;
+}
+
+var y = square(4);
+
+console.log(y);
+
+// Simple Substraction Function in Javascript
+function sub(a, b) {
+  return a-b
+  }
+  console.log(sub(150, 200))
+  
