902f13dac68cc07d7fdc6b8a9d5e37305de01455
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-06-13 11:08:38 -0400

    Went over Methods in JavaScript

---
+++ JavaScript/methods.js
@@ -0,0 +1,10 @@
+//Calling Methods: Semantics
+var fgImage = new SVGImageElement("drewRobert.png");
+var w = fgImage.getWidth();
+var h = fgImage.getHeight();
+
+// Methods 
+// Perform some(complex) operation 
+// Act on an object 
+// Can have parameters 
+// Example: fgImage.getWidth();
