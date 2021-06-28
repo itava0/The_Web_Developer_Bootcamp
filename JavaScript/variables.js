0c5a27f0a123fb9c7b62d8f8f68935a3fc2f1a0c
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-06-28 18:53:45 -0400

    Finished praticing string Methods

--- JavaScript/variables.js
+++ JavaScript/variables.js
@@ -9,7 +9,13 @@ let totalCar = greenCar + blueCar;
 //Our First String Variables
 let bestColor = "purple";
 let quote = 'you had me at "hello"';
-console.log(bestColor);
-console.log(quote);
+// console.log(bestColor);
+// console.log(quote);
 
 //String Methods 
+// DON'T TOUCH THIS LINE! (please)
+const word = "skateboard"; //Don't change this line!
+
+// YOUR CODE BELOW THIS LINE:
+let facialHair = word.slice(5).replace("board", "beard");
+console.log(facialHair);
