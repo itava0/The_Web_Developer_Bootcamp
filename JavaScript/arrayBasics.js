195a07f61b6abd2d43eb7af6b15ec8fc94fea6ad
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-07-01 19:12:13 -0400

    Went over commonly used array methods

--- JavaScript/arrayBasics.js
+++ JavaScript/arrayBasics.js
@@ -5,3 +5,23 @@ let colors = ["red", "orange", "yellow"];
 
 console.log(colors.length);
 console.log(`I love ${colors[2]}`);
+
+//Array Methods
+
+//Added a new color to the array
+colors.push("blue");
+colors.push("white");
+console.log(colors);
+
+
+//Removed the last color from array
+colors.pop();
+console.log(colors);
+
+//Removed the fist color from array
+colors.shift();
+console.log(colors);
+
+//Removed the fist color from array
+colors.unshift("pink");
+console.log(colors);
