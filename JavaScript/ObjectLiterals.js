a90751ba88e5d580fc805960e14b9aaea3b350ec
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-10-23 19:48:14 -0400

    Object Literals

---
+++ JavaScript/ObjectLiterals.js
@@ -0,0 +1,12 @@
+//Objects are collections of properties
+//Properties are a key value pair
+
+const person = {
+  firstName: 'Mick',
+  lastName: 'Jagger',
+  age: 27,
+  weight: 195,
+  height: 5.8
+}
+
+console.log(`what's your name? ${person.firstName}`);
