7cf395b7c5504fa9eba982788618e4e62edd4ec7
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2021-10-24 20:49:50 -0400

    Solved coding challenge

--- JavaScript/ObjectLiterals.js
+++ JavaScript/ObjectLiterals.js
@@ -10,3 +10,15 @@ const person = {
 }
 
 console.log(`what's your name? ${person.firstName}`);
+
+const restaurant = {
+  name: 'Ichiran Ramen',
+  address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+  city: 'Brooklyn',
+  state: 'NY',
+  zipcode: '11206',
+}
+
+let fullAddress = `${restaurant['address']}, ${restaurant['city']}, ${restaurant['state']}, ${restaurant['zipcode']} `;
+
+console.log(fullAddress);
