a2112d90760113be97cb4889f718b0574bc3827c
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-02-26 14:41:56 -0500

    Intro to For Loops

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -1,5 +1,9 @@
 //Repeating Steps: For Loops
-for ( var pixel of Image.values()) {
-  var newG = 255 - pixel.getGreen();
-  pixel.setGreen(newG);
+// for ( var pixel of Image.values()) {
+//   var newG = 255 - pixel.getGreen();
+//   pixel.setGreen(newG);
+// }
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
 }
