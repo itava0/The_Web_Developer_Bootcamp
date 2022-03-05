6b5dc2ae0a395d230dde48ed859d6301287188b4
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-03-05 13:04:49 -0500

    Looping over Arrays

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -7,3 +7,16 @@
 for (let i = 0; i <= 10; i++) {
   console.log(i);
 }
+
+const animals = ['lions', 'elephan', 'tiger', 'bear'];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log(animals[i])
+}
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; //DONT TOUCH THIS LINE!
+
+// WRITE YOUR LOOP BELOW THIS LINE:
+for(let i= 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase());
+}
