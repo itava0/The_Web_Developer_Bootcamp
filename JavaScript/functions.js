e5bacbec6a74a6b38aff5e79278d20180c35a1ad
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-03-06 12:02:28 -0500

    Intro to functions and arguments

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -16,3 +16,17 @@ function sub(a, b) {
   }
   console.log(sub(150, 200))
   
+  // Write your function here:
+function printHeart() {
+  console.log("<3");
+}
+
+printHeart();
+
+function rant(message) {
+  for (let i = 0; i < 3; i++) {
+     console.log(message.toUpperCase());
+  }
+}
+
+rant("I hate walking");
