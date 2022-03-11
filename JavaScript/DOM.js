ac1f22419f9613acf9e6c1900c773661d260e184
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-03-11 15:02:12 -0500

    Intro to the DOM

---
+++ JavaScript/DOM.js
@@ -0,0 +1,6 @@
+// How to select documents using an ID
+
+//Select the image element by its id
+const image = document.getElementById("unicorn");
+//Select the hearder element by its id
+const heading = document.getElementById("mainheading");
