ae49a07ed877eb185a43d1887e546bcc8b04b503
Ivan Tavarez <40846384+itava0@users.noreply.github.com>
2022-03-11 15:28:45 -0500

    Learned how to select elements using JS

--- JavaScript/DOM.js
+++ JavaScript/DOM.js
@@ -4,3 +4,11 @@
 const image = document.getElementById("unicorn");
 //Select the hearder element by its id
 const heading = document.getElementById("mainheading");
+//Select all the images byt its tag name.
+const allImages = document.getElementsByTagName("img");
+//Select the img element by its classname;
+const firstImage = document.getElementsByClassName("first_img");
+//Select all the elements by its classname
+const doneTodos = document.querySelectorAll(".done");
+//Select the checkbox element by its id
+const checkbox = document.querySelector("#scales");
